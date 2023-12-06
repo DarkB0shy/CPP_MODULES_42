095d8ebcfd09c7b2f4eddabcbf7813d01fbe6b30
Busto Arsizio <top@gmail.com>
2023-12-06 19:07:16 +0100

    Senigallia

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -1,13 +1,10 @@
 #include "PhoneBook.hpp"
-#include "PhoneBook.cpp"
-#include "Contact.cpp"
-#include "Contact.hpp"
 
 int main (void)
 {
 	PhoneBook Guarnito;
 
 	Guarnito.phoneBookLoop();
-	std::cout << std::endl;
+	std::cout << "Thanks!"<<std::endl;
 	return (0);
 }
